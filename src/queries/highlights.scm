39c4c9c828fe2c861c6ac96fef416dfb89b43d9b
Nick <rudbsm@gmail.com>
2025-10-08 11:18:01 +0300

    add highlights schema

---
+++ src/queries/highlights.scm
@@ -0,0 +1,42 @@
+[
+  "{%"
+  "%}"
+  "("
+  ")"
+] @punctuation.bracket
+
+(comment_block) @comment
+
+[
+  "if"
+  "else"
+  "switch"
+  "case"
+  "end"
+] @keyword
+
+;; Operators
+[
+  (binary_operator)
+  "!"
+] @operator
+
+;; Pipes
+(pipe_expression
+  "|" @operator
+  function: (identifier) @function)
+
+;; Function call
+(call_expression
+  function: (identifier) @function) 
+
+[
+  ","
+] @punctuation.delimiter
+
+;; Variables
+(identifier) @variable
+
+;; Literals
+(string_literal) @constant.builtin
+(boolean_literal) @constant.builtin
