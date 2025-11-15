84e49c93e6be8e96919821d7f19150b0aadf20eb
skewb1k <skewb1kunix@gmail.com>
2025-11-15 19:32:52 +0300

    fix(scanner): ensure spaces are skipped only after statements (#9)

--- src/scanner.c
+++ src/scanner.c
@@ -2,6 +2,7 @@
 
 enum TokenType {
   TEXT,
+  _NEWLINE,
 };
 
 void *tree_sitter_vie_external_scanner_create() { return NULL; }
@@ -12,48 +13,50 @@ unsigned tree_sitter_vie_external_scanner_serialize(void *p, char *b) {
 void tree_sitter_vie_external_scanner_deserialize(void *p, const char *b,
                                                   unsigned n) {}
 
+static inline void advance(TSLexer *l) { l->advance(l, false); }
+
 bool tree_sitter_vie_external_scanner_scan(void *p, TSLexer *l,
                                            const bool *valid_symbols) {
-  if (!valid_symbols[TEXT]) {
-    return false;
-  }
-
-  bool has_content = false;
-
-  while (l->lookahead == ' ' || l->lookahead == '\t') {
-    has_content = true;
-    l->advance(l, true);
-  }
-
-  if (l->lookahead == '\n') {
-    has_content = true;
-    l->advance(l, true);
-  } else if (l->lookahead == '\r') {
-    has_content = true;
-    l->advance(l, true);
-    if (l->lookahead == '\n')
-      l->advance(l, true);
-  }
-
-  while (!l->eof(l)) {
-    if (l->lookahead == '{') {
-      l->mark_end(l);
-      l->advance(l, false);
-      int next = l->lookahead;
-
-      if (next == '%' || next == '{' || next == '#') {
-        return has_content; // Do NOT emit if nothing was read
-      }
-    } else {
-      l->advance(l, false);
+  if (valid_symbols[_NEWLINE]) {
+    while (l->lookahead == ' ' || l->lookahead == '\t') {
+      advance(l);
     }
-    has_content = true;
-  }
 
-  if (!has_content) {
-    return false;
+    if (l->lookahead == '\n') {
+      advance(l);
+    } else if (l->lookahead == '\r') {
+      advance(l);
+      if (l->lookahead == '\n')
+        advance(l);
+    }
+
+    l->result_symbol = _NEWLINE;
+    l->mark_end(l);
+    return true;
+  } else if (valid_symbols[TEXT]) {
+    bool emit = false;
+
+    while (!l->eof(l)) {
+      if (l->lookahead == '{') {
+        l->mark_end(l);
+        advance(l);
+        int next = l->lookahead;
+
+        if (next == '%' || next == '{' || next == '#') {
+          return emit; // Do NOT emit if nothing was read
+        }
+      } else {
+        advance(l);
+      }
+      emit = true;
+    }
+
+    if (!emit) {
+      return false;
+    }
+    l->result_symbol = TEXT;
+    l->mark_end(l);
+    return true;
   }
-  l->result_symbol = TEXT;
-  l->mark_end(l);
-  return true;
+  return false;
 }
