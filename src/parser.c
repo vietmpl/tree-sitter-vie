f4f138cee51efe6ef6bf3d5a858c17ce00bda28f
skewb1k <skewb1kunix@gmail.com>
2025-11-15 19:42:03 +0300

    release: v0.2.3

--- src/parser.c
+++ src/parser.c
@@ -3003,7 +3003,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_vie(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 2,
-      .patch_version = 2,
+      .patch_version = 3,
     },
   };
   return &language;
